dfd0a44fd8401abfb42fb377772e1712e747aae9
lam1106 <nguyen.lamtung.qb@gmail.com>
2024-04-22 20:13:51 +0700

    SAM

--- chiadetri/quicksort.cpp
+++ chiadetri/quicksort.cpp
@@ -1,165 +1,95 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
-struct edge
-{
-    int a, b;
-    int w;
-
-    edge(int x, int y, int z)
-    {
-        this->a = x;
-        this->b = y;
-        this->w = z;
-    }
-};
-vector<edge> canhs;
-int n = 6, m = 9;
-const int maxn = 1001;
-int parent[maxn], sz[maxn];
-void make_set()
-{
-    for (int i = 0; i <= n; i++)
-    {
-        parent[i] = i;
-        sz[i] = 1;
-    }
+void swap(vector<int> &items, int leftIndex, int rightIndex) {
+    int temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
 }
-void printCycle(int a, int b, int parent[])
-{
-    cout << "Cycle found: ";
-    vector<int> cycleNodes;
-    cycleNodes.push_back(a);
-    int current = b;
-    while (current != a)
-    {
-        cycleNodes.push_back(current);
-        current = parent[current];
-    }
-    cycleNodes.push_back(a); // Add the starting node to close the cycle
-    for (int i = 0; i < cycleNodes.size(); ++i)
-    {
-        if (i != cycleNodes.size() - 1)
-            cout << cycleNodes[i] << " -> ";
-        else
-            cout << cycleNodes[i];
+
+int partition(vector<int> &items, int left, int right) {
+    int pivot = items[(right + left) / 2];
+    int i = left;
+    int j = right;
+
+    cout << "Pivot: " << pivot << endl;
+    cout << "Current array: ";
+    for (int k = left; k <= right; k++) {
+        cout << items[k] << " ";
     }
     cout << endl;
-}
 
-int find(int v)
-{
-    if (v == parent[v])
-        return v;
-
-    return parent[v] = find(parent[v]);
-}
-
-bool Union(int a, int b)
-{
-    int atest = a;
-    int btest = b;
-    a = find(a);
-    b = find(b);
-
-    if (a == b)
-    {
-
-        cout << "Vi canh (" << atest << ";" << btest << ") tao thanh chu trinh nen giu nguyen T";
-
-        return false;
-    }
-
-    if (sz[a] < sz[b])
-    {
-        swap(a, b);
-    }
-
-    parent[b] = a;
-    sz[a] + sz[b];
-    return true;
-}
-bool cmp(edge a, edge b)
-{
-    return a.w < b.w;
-}
-
-void kruskal()
-{
-    vector<edge> mst;
-    int d = 0;
-
-    sort(canhs.begin(), canhs.end(), cmp);
-    cout << "Canh e sau khi sap xep: {";
-    for (auto s : canhs)
-    {
-        cout << "(" << s.a << "," << s.b << "," << s.w << "),";
-    }
-    cout << "}";
-    for (int i = 0; i < m; i++)
-    {
-        if (mst.size() == m - 1)
-            break;
-        cout << "\n|T| = " << mst.size() << " <= " << n - 1 << endl;
-        edge e = canhs[i];
-        if (Union(e.a, e.b))
-        {
-            cout << "\n E  = E \\ (" << e.a << ";" << e.b << ") = {";
-            for (int j = i + 1; j < m; j++)
-            {
-                cout << "(" << canhs[j].a << "," << canhs[j].b << "," << canhs[j].w << "),";
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j);
+            cout << "Swapped " << items[i] << " and " << items[j] << ": ";
+            for (int k = left; k <= right; k++) {
+                cout << items[k] << " ";
             }
-            cout << "}";
-            mst.push_back(e);
-            cout << "\n T  = {";
-            for (auto mstnode : mst)
-            {
-                cout << "(" << mstnode.a << "," << mstnode.b << "," << mstnode.w << "),";
-            }
-            cout << "}";
-            d += e.w;
+            cout << endl;
+            i++;
+            j--;
         }
     }
-    if (mst.size() != n - 1)
-    {
-        cout << "Do thi khong ton tai";
-    }
-    else
-    {
-        cout << "\nMST: " << d << endl;
-        for (auto x : mst)
-        {
-            cout << x.a << " " << x.b << " " << x.w << endl;
+
+    return i;
+}
+
+void quickSort(vector<int> &items, int left, int right) {
+    if (left < right) {
+        int index = partition(items, left, right);
+
+        vector<int> leftArray(items.begin() + left, items.begin() + index);
+        vector<int> rightArray(items.begin() + index, items.begin() + right + 1);
+
+        cout << "Section 1: [";
+        for (int num : leftArray) {
+            cout << num << " ";
+        }
+        cout << "]" << endl;
+
+        cout << "Section 2: [";
+        for (int num : rightArray) {
+            cout << num << " ";
+        }
+        cout << "]" << endl << endl;
+
+        if (left < index - 1) {
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(items, index, right);
         }
     }
 }
 
-int main()
-{
-    canhs.push_back(edge(1, 2, 33));
-    canhs.push_back(edge(1, 3, 17));
-    canhs.push_back(edge(2, 3, 18));
-    canhs.push_back(edge(2, 4, 20));
-    canhs.push_back(edge(3, 4, 16));
-    canhs.push_back(edge(3, 5, 4));
-    canhs.push_back(edge(4, 6, 8));
-    canhs.push_back(edge(5, 6, 14));
-    canhs.push_back(edge(4, 5, 9));
+int main() {
+    vector<int> items = {42, 23, 74, 11, 65, 58, 94, 36, 99, 87};
+    // vector<int> items = {5, 3, 7, 6, 2, 9};
 
-    // for (int i = 0; i < m; i++)
-    // {
-    //     int x, y, z;
-    //     cout << "Nhap dinh dau: ";
-    //     cin >> x;
-    //     cout << "Nhap dinh cuoi: ";
-    //     cin >> y;
-    //     cout << "Nhap trong so: ";
-    //     cin >> z;
+    cout << "Initial array:" << endl;
+    for (int num : items) {
+        cout << num << " ";
+    }
+    cout << endl << endl;
 
-    //     canhs.push_back(e);
-    // }
-    make_set();
-    kruskal();
+    cout << "Sorting process:" << endl << endl;
+    quickSort(items, 0, items.size() - 1);
+
+    cout << endl << "Sorted array:" << endl;
+    for (int num : items) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
+
